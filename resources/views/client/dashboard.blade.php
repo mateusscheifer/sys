4baf5625ea9becba6a8329a5a31c4d18a46db2a7
nicollasmb <92215186+nicollasmb@users.noreply.github.com>
2024-11-08 23:11:03 -0300

    Fix no AppShell: - Responsividade dos componentes renderizados dentro do app-layout - Removido z-index da navbar - Ajuste no Header do app-layout para a NavBar rodar dentro dele (para o z-index não ser necessario)

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -1,9 +1,3 @@
 <x-app-layout>
-    <div class="py-12 mt-14">
-        <div class=" mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg ml-64">
-                @livewire('client.dashboard')
-            </div>
-        </div>
-    </div>
+    @livewire('client.dashboard')
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,24 +26,25 @@
 {{--            @livewire('navigation-menu')--}}
 
             <!-- Page Heading -->
-            @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
+                        <x-navbar></x-navbar>
                 </header>
-            @endif
-
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                <div class="py-12 mt-14">
+                    <div class=" mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg md:ml-64">
+                            {{ $slot }}
+                        </div>
+                    </div>
+                </div>
             </main>
         </div>
 
 
     <!-- Tudo isso aqui é o Appshell -->
     <div class="antialiased bg-gray-50 dark:bg-gray-900">
-<x-navbar></x-navbar>
+
 
         <!-- Sidebar -->
 
